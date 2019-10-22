d4c741c21c9c9cbabfe2f7672199cc325f4c837f
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-22 13:01:38 +0100

    Update base.pp
    Add gpgkey

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -6,10 +6,11 @@ class profile::base {
   
   # include epel-release repo to get access to htop package
   yumrepo {'epel-release':
-    enabled => 1,
-    baseurl => 'https://dl.fedoraproject.org/pub/epel/7/x86_64',
-    descr   => 'Extra Packages for Enterprise Linux repository configuration',
+    enabled  => 1,
+    baseurl  => 'https://dl.fedoraproject.org/pub/epel/7/x86_64',
+    descr    => 'Extra Packages for Enterprise Linux repository configuration',
     gpgcheck => 1,
+    gpgkey   => 'https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7'
   }
   
   package {'htop':
