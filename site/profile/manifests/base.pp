bb86105f787bec726989dc8addb07927b2a42578
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-22 15:44:58 +0100

    Update base.pp
    Add cron jobs to perform yum clean and yum update

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -4,6 +4,23 @@ class profile::base {
   }
   include profile::ssh_server
   
+  # perform yum clean and update with cron
+  cron {'yum-clean':
+    command => '/bin/yum -y -q clean all',
+    user    => 'root',
+    hour    => 0,
+    minute  => 0,
+    weekday => 0,
+  }
+  
+  cron {'yum-update':
+    command => '/bin/yum -y -q update',
+    user    => 'root',
+    hour    => 1,
+    minute  => 0,
+    weekday => 1,
+  }
+  
   # include epel-release repo to get access to htop package
   yumrepo {'epel-release':
     enabled  => 1,
