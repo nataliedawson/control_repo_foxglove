19a8c4bd5d2501d02d863a93bcb33acfac3fb157
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-22 12:47:36 +0100

    Update base.pp
    Update baseurl

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -7,9 +7,8 @@ class profile::base {
   # include epel-release repo to get access to htop package
   yumrepo {'epel-release':
     enabled => 1,
-    baseurl => 'https://dl.fedoraproject.org/pub/epel',
+    baseurl => 'https://dl.fedoraproject.org/pub/epel/7/x86_64',
     name    => 'epel-release-latest-7.noarch.rpm',
     descr   => 'Extra Packages for Enterprise Linux repository configuration',
-    ensure  => present,
   }
 }
