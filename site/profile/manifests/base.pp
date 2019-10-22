63655d32a563da4c22f7c6771eba8e4ed9c2e27a
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-22 12:27:41 +0100

    Update base.pp
    Add epel-release yumrepo to gain access to htop package

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,4 +3,11 @@ class profile::base {
     ensure => present,
   }
   include profile::ssh_server
+  
+  # include epel-release repo to get access to htop package
+  yumrepo {'epel-release':
+    enabled => 1,
+    descr   => 'Extra Packages for Enterprise Linux repository configuration'
+    ensure  => present
+  }
 }
