69cb1f3bd095bfde4a4f2e9fa497633652b09ffc
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-24 12:45:27 +0100

    Update site.pp
    Try moving order of ntp class definitions to fix yum package issues

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,8 @@
 node default {
+  class {'ntp':
+    servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
+  }
+  
   file {'/root/README':
     ensure  => file,
     content => 'This is a readme',
@@ -7,33 +11,38 @@ node default {
 }
 
 node 'master.puppet.vm' {
+  class {'ntp':
+    servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
+  }
+  
   include role::master_server
+  
   file {'/root/README':
     ensure => file,
     content => "Welcome to ${fqdn}\n",
   }
-  class {'ntp':
-    servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
-  }
 }
 
 node /^web/ {
-  include role::app_server
   class { 'ntp':
         servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
   }
+  
+  include role::app_server
 }
 
 node /^db/ {
-  include role::db_server
   class { 'ntp':
         servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
   }
+  
+  include role::db_server
 }
 
 node /^foxglove/ {
-  include role::foxglove_server
   class { 'ntp':
         servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
   }
+  
+  include role::foxglove_server
 }
