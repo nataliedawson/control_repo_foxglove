75e1c597c834e1a3d094cb674f3c0e39bfe70e8b
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-24 14:14:47 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,7 +7,7 @@ node default {
   
   file {'/root/README':
     ensure  => file,
-    content => 'This is a readme',
+    content => 'This is a readme generated for a default node',
     owner   => 'root',
   }
 }
@@ -27,6 +27,11 @@ node /^web/ {
   class {'ntp':
     servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
     enable  => true,
+  }
+  
+  file {'/root/README':
+    ensure => file,
+    content => "Welcome to ${fqdn}\n",
   }
 }
 
