043f05d3da18e538440aaa6d7b243391c0b5f389
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-24 13:13:06 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,10 +11,6 @@ node default {
 }
 
 node 'master.puppet.vm' {
-  class {'ntp':
-    servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
-  }
-  
   include role::master_server
   
   file {'/root/README':
@@ -24,25 +20,13 @@ node 'master.puppet.vm' {
 }
 
 node /^web/ {
-  class { 'ntp':
-        servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
-  }
-  
   include role::app_server
 }
 
 node /^db/ {
-  class { 'ntp':
-        servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
-  }
-  
   include role::db_server
 }
 
 node /^foxglove/ {
-  class { 'ntp':
-        servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
-  }
-  
   include role::foxglove_server
 }
