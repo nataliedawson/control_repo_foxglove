9855c1c27870397738d542f857e2bab268093df3
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-24 13:14:43 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,8 @@
 node default {
-  class {'ntp':
-    servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
-  }
+  
+  #class {'ntp':
+  #  servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
+  #}
   
   file {'/root/README':
     ensure  => file,
