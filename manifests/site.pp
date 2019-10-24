ca903b994eec56e475b9e65c9755c1ea13ff2f53
nataliedawson <nataliedawson@users.noreply.github.com>
2019-10-24 12:32:25 +0100

    Update site.pp
    Add class ntp explicitly to each node

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,22 +5,35 @@ node default {
     owner   => 'root',
   }
 }
+
 node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
     content => "Welcome to ${fqdn}\n",
   }
+  class {'ntp':
+    servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu'],
+  }
 }
 
 node /^web/ {
   include role::app_server
+  class { 'ntp':
+        servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
+  }
 }
 
 node /^db/ {
   include role::db_server
+  class { 'ntp':
+        servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
+  }
 }
 
 node /^foxglove/ {
   include role::foxglove_server
+  class { 'ntp':
+        servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
+  }
 }
