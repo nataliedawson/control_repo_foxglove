371bb607a07495fab631e851f936d30a73a6a000
nataliedawson <nataliedawson@users.noreply.github.com>
2019-11-15 15:23:45 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,28 +10,6 @@ node default {
   }
 }
 
-node 'master.puppet.vm' {
-  include role::master_server
-  
-  file {'/root/README':
-    ensure => file,
-    content => "Welcome to ${fqdn}\n",
-  }
-}
-
-node /^web/ {
-  include role::app_server
-  
-  file {'/root/README':
-    ensure => file,
-    content => "Welcome to ${fqdn}\n",
-  }
-}
-
-node /^db/ {
-  include role::db_server
-}
-
 node /^foxglove/ {
   include role::foxglove_server
 }
