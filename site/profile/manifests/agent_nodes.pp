033401a7b6e01ac5b1f7a72668cdc2a6ef740ddb
nataliedawson <nataliedawson@users.noreply.github.com>
2020-02-20 13:40:29 +0000

    Update agent_nodes.pp
    Add in additional directory path checks.

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,14 +4,25 @@ class profile::agent_nodes {
     ip     => '128.16.5.105',
   }
   
-  # ensure that the expected directory structure is in place
+  # check that the expected directory structures are in place
   # for the webservices jobs
   file {'/grid/gridstore2/cathgrid/nr':
     ensure => directory,
   }
   
+  file {'/grid/gridstore2/cathgrid/WebServices':
+    ensure => directory,
+  }
   
-  # ensure that the expected directory structure is in place
+  file {'/grid/gridstore2/cathgrid/WebServicesArchive':
+    ensure => directory,
+  }
+  
+  file {'/grid/gridstore2/cathgrid/WebServicesTemp':
+    ensure => directory,
+  }
+  
+  # check that the expected directory structure is in place
   # for hosting input data
   file {'/grid/gridstore2/cath/data/current':
     ensure => directory,
