005eea55793336c868d4dcae88ef1c0ae08c8076
nataliedawson <nataliedawson@users.noreply.github.com>
2020-08-20 11:36:47 +0100

    Update agent_nodes.pp
    Add host alias for foxglove in /etc/hosts.

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,7 +1,8 @@
 class profile::agent_nodes {
   host {'foxglove.cs.ucl.ac.uk':
-    ensure => present,
-    ip     => '128.16.5.105',
+    ensure       => present,
+    ip           => '128.16.5.105',
+    host_aliases => 'foxglove',
   }
   
   # nr file names
